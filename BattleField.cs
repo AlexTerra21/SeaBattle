7d9706c660e8e75b6ffbf65c090d1be2e056f83c
AlexTerra21 <baewoo21@gmail.com>
2017-04-13 16:58:36 +0300

    Первый коммит, создание репозитория

---
+++ BattleField.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeaBattle
+{
+    public class BattleFieldС
+    {
+        public string[,] BattleField = new string[10, 10];
+    } // Batlefild
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeaBattle
+{
+    class Program
+    {
+        static bool[,] BattleField = new bool[10,10];
+        static Random rnd = new Random();
+
+        static void Main(string[] args)
+        {
+           // FillBattleField();
+            PrintBattleFild();
+
+            
+
+            Console.ReadLine();
+        }
+
+        static void FillBattleField()
+        {
+            for (int i = 0; i < 9; i++)
+                for (int j = 0; j < 9; j++)
+                {
+                    BattleField[i, j] = rnd.Next(2)==0?false:true;
+                }
+        }
+
+        static void PrintBattleFild()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    Console.Write(BattleField[i, j]?"*":"_");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
